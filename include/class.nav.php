4abbbc6faa53d0b6322a2ca815d118f24149e766
Jonathan Steen <therightjon@gmail.com>
2024-01-10 09:29:16 -0600

    Nav change

--- include/class.nav.php
+++ include/class.nav.php
@@ -328,7 +328,7 @@ class UserNav {
 
             $navs = array();
             $user = $this->user;
-            $navs['home']=array('desc'=>__('Support Center Home'),'href'=>'index.php','title'=>'');
+            $navs['home']=array('desc'=>__('OBGYN Helpdesk Home'),'href'=>'index.php','title'=>'');
             if($cfg && $cfg->isKnowledgebaseEnabled())
                 $navs['kb']=array('desc'=>__('Knowledgebase'),'href'=>'kb/index.php','title'=>'');
 
